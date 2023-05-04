c15a27f3da1249401856233b61993d35c292518c
Amar Diyab <diyabamar3043@gmail.com>
2023-05-04 17:21:13 +0530

    Fixed

--- src/components/account.js
+++ src/components/account.js
@@ -3,23 +3,23 @@ import { useSession, getSession } from 'next-auth/react'
 import Dashboard from '../pages/dashboard'
 import Login from '../pages/login'
 
-export const Account = () => {
+// export const Account = () => {
 
-    const { data: session, status } = useSession({ required: true })
+//     const { data: session, status } = useSession({ required: true })
 
-    if (status === 'authenticated') {
-        return (
-            <Dashboard />
-        )
-    } else {
-        return (
-            <Login />
-        )
-    }
-}
+//     if (status === 'authenticated') {
+//         return (
+//             <Dashboard />
+//         )
+//     } else {
+//         return (
+//             <Login />
+//         )
+//     }
+// }
 
 //Server Side
-export const getServerSideProps = async (context) => {
+const getServerSideProps = async (context) => {
     const session = await getSession(context)
     
     if (!session) {
@@ -34,3 +34,5 @@ export const getServerSideProps = async (context) => {
 
     }
 }
+
+export default getServerSideProps
