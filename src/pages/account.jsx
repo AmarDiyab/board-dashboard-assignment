e9e06c073cc2c38146a064b3ec0225798e3dd44f
Amar Diyab <diyabamar3043@gmail.com>
2023-05-04 16:45:43 +0530

    deployment fixes

---
+++ src/pages/account.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useSession, signIn, signOut, getSession } from 'next-auth/react'
+import Dashboard from './dashboard'
+import Login from './login'
+
+export const Account = () => {
+
+    const { data: session, status } = useSession({ required: true })
+
+    if (status === 'authenticated') {
+        return (
+            <Dashboard />
+        )
+    } else {
+        return (
+            <Login />
+        )
+    }
+}
+
+//Server Side
+export const getServerSideProps = async (context) => {
+    const session = await getSession(context)
+    
+    if (!session) {
+        return {
+            redirect: {
+                destination: '/login'
+            }
+        }
+    }
+    return {
+        props: {session},
+
+    }
+}
